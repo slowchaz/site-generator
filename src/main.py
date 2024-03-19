8bef7a2c8adcdc371510c28136383a1ad43d941f
chaz <corfitzgerald@gmail.com>
2024-03-19 14:46:06 -0500

    basic inline markdown splitting

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode, text_node_to_html_node
+from textnode import TextNode, text_node_to_html_node, split_nodes_delimiter
 from htmlnode import HTMLNode, LeafNode, ParentNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
 
 def main():
     print("main")
-    text_node = TextNode("just some text", "text")
-    print(text_node_to_html_node(text_node))
+    
+    node = TextNode("This is a text with a `code block` word", text_type_text)
+    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    print(new_nodes)
+
+
 main()
